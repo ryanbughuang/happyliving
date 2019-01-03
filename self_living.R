3a4f3a5c25fe24072f99ed4821a5755395df28fd
ryanhuang <ryanhuang970623@gmail.com>
2019-01-03 13:57:59 +0800

    basic info update

--- self_living.R
+++ self_living.R
@@ -1,4 +1,11 @@
 path = '/Users/ryanhuang/Desktop/107-1/business statistics/happyliving/happylivingdata_allvalid.csv'
 info = read.table(path, header = TRUE, sep = ',',fileEncoding='big5')
-colname = (colnames(info))
-self_living = info[ info$n3_self_living > 0,]
+info$response = c(1:68323)
+self_living = info[ info$n3_self_living > 0, c(22:27,282)]
+View(self_living)
+colnames(basic_info)
+basic_info = info[,269:279] #只選填答者基本資料
+#apply(basic_info[,c(4:8)],1,'sum') #living status detail是複選^^
+basic_info = subset(basic_info, select = c(1:3,11)) #只選性別、年齡、住況、住地
+
+
